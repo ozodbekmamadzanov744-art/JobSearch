a10a26d399485ea32ceedf27028bd09f2639d0d2
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-07-10 17:10:15 +0600

    добавлена модель WorkExperienceInfo

---
+++ src/main/java/kg/attractor/jobsearch/model/ModelWorkExperienceInfo.java
@@ -0,0 +1,18 @@
+package kg.attractor.jobsearch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModelWorkExperienceInfo {
+
+    private Long id;
+    private Long resumeId;
+    private Integer years;
+    private String companyName;
+    private String position;
+    private String responsibilities;
+}
