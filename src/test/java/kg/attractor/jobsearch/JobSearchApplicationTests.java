3afb93648107272c05d37591d1c00caf7df7a14a
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-07-10 11:17:51 +0600

    first commits

---
+++ src/test/java/kg/attractor/jobsearch/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.jobsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
