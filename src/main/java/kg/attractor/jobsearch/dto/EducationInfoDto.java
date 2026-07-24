f3d200d937d8c434f56914a1d1d2a60e442157ce
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-07-24 17:41:51 +0600

    добавлена валидация для DTO резюме и его модулей

--- src/main/java/kg/attractor/jobsearch/dto/EducationInfoDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/EducationInfoDto.java
@@ -1,5 +1,7 @@
 package kg.attractor.jobsearch.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,18 @@ import java.time.LocalDate;
 public class EducationInfoDto {
 
     private Long id;
+
+    @NotBlank(message = "Учебное заведение обязательно для заполнения")
     private String institution;
+
+    @NotBlank(message = "Программа обучения обязательна для заполнения")
     private String program;
+
+    @NotNull(message = "Дата начала обучения обязательна")
     private LocalDate startDate;
+
     private LocalDate endDate;
+
+    @NotBlank(message = "Степень обязательна для заполнения")
     private String degree;
 }
--- src/main/java/kg/attractor/jobsearch/dto/WorkExperienceInfoDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/WorkExperienceInfoDto.java
@@ -1,5 +1,8 @@
 package kg.attractor.jobsearch.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,17 @@ import lombok.NoArgsConstructor;
 public class WorkExperienceInfoDto {
 
     private Long id;
+
+    @NotNull(message = "Количество лет опыта обязательно")
+    @PositiveOrZero(message = "Количество лет опыта не может быть отрицательным")
     private Integer years;
+
+    @NotBlank(message = "Название компании обязательно для заполнения")
     private String companyName;
+
+    @NotBlank(message = "Должность обязательна для заполнения")
     private String position;
+
+    @NotBlank(message = "Обязанности обязательны для заполнения")
     private String responsibilities;
 }
