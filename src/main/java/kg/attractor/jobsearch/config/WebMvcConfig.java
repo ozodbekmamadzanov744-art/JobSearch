6d15009b236e7a6accd9e506e5e83d9a5870a6f7
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-09-04 18:09:22 +0600

    Добавлена настройка локализации и переключение языка

--- src/main/java/kg/attractor/jobsearch/config/WebMvcConfig.java
+++ src/main/java/kg/attractor/jobsearch/config/WebMvcConfig.java
@@ -1,12 +1,44 @@
 package kg.attractor.jobsearch.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver =
+                new CookieLocaleResolver("jobSearchLocale");
+
+        localeResolver.setDefaultLocale(Locale.forLanguageTag("ru"));
+        localeResolver.setCookieMaxAge(Duration.ofDays(365));
+        localeResolver.setCookiePath("/");
+
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
